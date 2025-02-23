edf87802deba9700d594c930febd277adc14517d
Naveen <naveensbsf123@gmail.com>
2025-02-23 21:17:22 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -64,7 +64,7 @@ const handleSubmit=async(e)=>{
     chatContainer.scrollTop=chatContainer.scrollHeight;
     const messageDiv=document.getElementById(uniqueId);
     loader(messageDiv);
-const response= await fetch('http://localhost:5000',{
+const response= await fetch('https://nocode-fyg5.onrender.com',{
     method:'POST',
     headers:{
        'Content-Type':'application/json' 
